39af12a0eb40448aeb4b43fa42ad5c2a7137218f
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-07-26 20:06:21 -0500

    error handling for invalid tokens

--- src/middlewares/handleErrors.js
+++ src/middlewares/handleErrors.js
@@ -2,7 +2,9 @@ const ERROR_HANDLERS = {
   defaultError: res => res.status(500).end(),
   MongoError: (res, err) =>
     res.status(409).send({error: err.message}),
-  ValidationError: (res, err) => res.status(409).send({error: err.message})
+  ValidationError: (res, err) => res.status(409).send({error: err.message}),
+  JsonWebTokenError: (res) =>
+    res.status(401).json({error: 'Token missing or invalid'})
 };
 
 module.exports = (error, req, res, next) => {
