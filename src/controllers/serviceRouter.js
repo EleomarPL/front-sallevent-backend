856bfb2fac70b8d3379871fc318d330323be8a41
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-08-18 12:59:28 -0500

    functionality quotation for service controller

--- src/controllers/serviceRouter.js
+++ src/controllers/serviceRouter.js
@@ -1,6 +1,7 @@
 const serviceRouter = require('express').Router();
 
 const Service = require('../models/Services');
+const Room = require('../models/Room');
 const adminStractor = require('../middlewares/adminStractor');
 
 serviceRouter.get('/get-all-services', async(req, res) => {
@@ -35,6 +36,52 @@ serviceRouter.put('/edit-service', adminStractor, async(req, res, next) => {
     };
     const savedChangeUser = await Service.findByIdAndUpdate(id, editService, {new: true});
     res.send(savedChangeUser);
+  } catch (err) {
+    next(err);
+  }
+});
+serviceRouter.post('/quotation', async(req, res, next) => {
+  const { listServices = [], timeStart, timeEnd} = req.body;
+  let idRoom = process.env.ID_ROOM;
+  try {
+    if (!(listServices && timeStart !== undefined && timeEnd !== undefined)) {
+      return res.status(400).json({
+        error: 'All parameters are required'
+      });
+    } else if (isNaN(timeStart) || isNaN(timeEnd)) {
+      return res.status(400).json({
+        error: 'The hours are not valid'
+      });
+    } else if ((timeStart < 0 || timeStart > 24) && (timeEnd < 0 || timeEnd > 24)) {
+      return res.status(400).json({
+        error: 'Invalid hours'
+      });
+    } else if (timeStart >= timeEnd) {
+      return res.status(400).json({
+        error: 'The reservation time range is not valid'
+      });
+    }
+
+    const roomData = await Room.findById(idRoom);
+    if (!roomData) {
+      return res.status(500).json({
+        error: 'Fail to find the room'
+      });
+    }
+    let totalServices = 0;
+    let totalTimeReservation = 0;
+    let total = 0;
+
+    listServices.forEach(element => {
+      if (Number(element.totalService))
+        totalServices += Number(element.totalService);
+    });
+    
+    totalTimeReservation += (timeEnd - timeStart) * roomData.priceHour;
+
+    total = totalServices + totalTimeReservation;
+    res.send({total});
+
   } catch (err) {
     next(err);
   }
